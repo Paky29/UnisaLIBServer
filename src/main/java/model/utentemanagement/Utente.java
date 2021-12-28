229042b18de0b0214463efd116ee90e479a2871b
d-donia <d.donia@studenti.unisa.it>
2021-12-28 18:06:58 +0100

    Aggiunta builder a Libro e Utente

--- src/main/java/model/utentemanagement/Utente.java
+++ src/main/java/model/utentemanagement/Utente.java
@@ -42,88 +42,129 @@ public class Utente {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getPassword() {
+        return password;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public String getCognome() {
         return cognome;
-    }
-
-    public void setCognome(String cognome) {
-        this.cognome = cognome;
     }
 
     public String getMatricola() {
         return matricola;
     }
 
-    public void setMatricola(String matricola) {
-        this.matricola = matricola;
-    }
-
     public String getGenere() {
         return genere;
-    }
-
-    public void setGenere(String genere) {
-        this.genere = genere;
     }
 
     public int getEta() {
         return eta;
     }
 
-    public void setEta(int eta) {
-        this.eta = eta;
-    }
-
     public boolean isAdmin() {
         return admin;
-    }
-
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public ArrayList<Libro> getInteressi() {
         return interessi;
     }
 
-    public void setInteressi(ArrayList<Libro> interessi) {
-        this.interessi = interessi;
-    }
-
     public ArrayList<Prestito> getPrestiti() {
         return prestiti;
-    }
-
-    public void setPrestiti(ArrayList<Prestito> prestiti) {
-        this.prestiti = prestiti;
     }
 
     public ArrayList<Prenotazione> getPrenotazioni() {
         return prenotazioni;
     }
 
-    public void setPrenotazioni(ArrayList<Prenotazione> prenotazioni) {
-        this.prenotazioni = prenotazioni;
+    public static class UtenteBuilder{
+        private String email, password, nome, cognome, matricola, genere;
+        private int eta;
+        private boolean admin;
+        private ArrayList<Libro> interessi;
+        private ArrayList<Prestito> prestiti;
+        private ArrayList<Prenotazione> prenotazioni;
+
+        public UtenteBuilder email(String email){
+            this.email=email;
+            return this;
+        }
+
+        public UtenteBuilder password(String password){
+            this.password=password;
+            return this;
+        }
+
+        public UtenteBuilder nome(String nome){
+            this.nome=nome;
+            return this;
+        }
+
+        public UtenteBuilder cognome(String cognome){
+            this.cognome=cognome;
+            return this;
+        }
+
+        public UtenteBuilder matricola(String matricola){
+            this.matricola=matricola;
+            return this;
+        }
+
+        public UtenteBuilder genere(String genere){
+            this.genere=genere;
+            return this;
+        }
+
+        public UtenteBuilder eta(int eta){
+            this.eta=eta;
+            return this;
+        }
+
+        public UtenteBuilder admin(boolean admin){
+            this.admin=admin;
+            return this;
+        }
+
+        public UtenteBuilder interessi(ArrayList<Libro> interessi){
+            this.interessi=interessi;
+            return this;
+        }
+
+        public UtenteBuilder prestiti(ArrayList<Prestito> prestiti){
+            this.prestiti=prestiti;
+            return this;
+        }
+
+        public UtenteBuilder prenotazioni(ArrayList<Prenotazione> prenotazioni){
+            this.prenotazioni=prenotazioni;
+            return this;
+        }
+
+        public Utente build(){
+            return new Utente(this);
+        }
+
+
+    }
+
+    private Utente(UtenteBuilder ub){
+        this.email=ub.email;
+        this.password=ub.password;
+        this.nome=ub.nome;
+        this.cognome=ub.cognome;
+        this.matricola=ub.matricola;
+        this.genere=ub.genere;
+        this.eta=ub.eta;
+        this.admin=ub.admin;
+        this.interessi=ub.interessi;
+        this.prestiti=ub.prestiti;
+        this.prenotazioni=ub.prenotazioni;
+
     }
 
     public String toJson(){
