85a1908121ea86b4e8fef17fa627cbd0aab6208d
Paky29 <p.somma11@studenti.unisa.it>
2021-12-29 16:04:19 +0100

    metodi resi statici

--- src/main/java/utility/SwitchDate.java
+++ src/main/java/utility/SwitchDate.java
@@ -6,12 +6,12 @@ import java.util.GregorianCalendar;
 
 public class SwitchDate {
 
-    public Date toDate(GregorianCalendar gc){
+    public static Date toDate(GregorianCalendar gc){
         Date d = new Date(gc.get(Calendar.YEAR), gc.get(Calendar.MONTH), gc.get(Calendar.DATE));
         return d;
     }
 
-    public GregorianCalendar toGregorianCalendar(Date d){
+    public static GregorianCalendar toGregorianCalendar(Date d){
         GregorianCalendar gc = new GregorianCalendar();
         gc.set(d.getYear(), d.getMonth(), d.getDate());
         return gc;
