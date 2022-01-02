520cb231260d05bc07894fc7016e47099ca5b94f
Paky29 <p.somma11@studenti.unisa.it>
2022-01-02 16:26:16 +0100

    estrazione dati utente dal database

--- src/main/java/model/utentemanagement/UtenteDAO.java
+++ src/main/java/model/utentemanagement/UtenteDAO.java
@@ -1,12 +1,18 @@
 package model.utentemanagement;
 
 import model.libromanagement.Libro;
+import model.libromanagement.LibroDAO;
+import model.prenotazionemanagement.Prenotazione;
+import model.prenotazionemanagement.PrenotazioneDAO;
+import model.prestitomanagement.Prestito;
+import model.prestitomanagement.PrestitoDAO;
 import utility.ConPool;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UtenteDAO {
@@ -21,6 +27,28 @@ public class UtenteDAO {
             if(rs.next())
                 u=UtenteExtractor.extract(rs);
 
+            return u;
+        }
+    }
+
+    public Utente doRetrieveByEmailAndPasswordAll(String email, String password) throws SQLException {
+        try(Connection conn= ConPool.getConnection()){
+            PreparedStatement ps=conn.prepareStatement("SELECT u.email, u.pword, u.nome, u.cognome, u.is_admin, u.is_nuovo, u.eta, u.genere, u.matricola FROM Utente u" +
+                    " WHERE u.email=? AND u.pword=?");
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            Utente u=null;
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                PrestitoDAO prestitoDAO=new PrestitoDAO();
+                LibroDAO libroDAO=new LibroDAO();
+                PrenotazioneDAO prenotazioneDAO=new PrenotazioneDAO();
+                ArrayList<Prestito> prestiti=prestitoDAO.doRetrieveByUtente(email);;
+                ArrayList<Prenotazione> prenotazioni=prenotazioneDAO.doRetrieveValidByUtente(email);
+                ArrayList<Libro> interesse= libroDAO.doRetrieveInteresse(email);;
+                u = UtenteExtractor.extract(rs,prestiti, prenotazioni, interesse);
+            }
             return u;
         }
     }
--- src/main/java/model/utentemanagement/UtenteExtractor.java
+++ src/main/java/model/utentemanagement/UtenteExtractor.java
@@ -1,7 +1,12 @@
 package model.utentemanagement;
 
+import model.libromanagement.Libro;
+import model.prenotazionemanagement.Prenotazione;
+import model.prestitomanagement.Prestito;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UtenteExtractor {
     public static Utente extract(ResultSet rs) throws SQLException {
@@ -15,6 +20,24 @@ public class UtenteExtractor {
                 eta(rs.getInt("u.eta")).
                 genere(rs.getString("u.genere")).
                 matricola(rs.getString("u.matricola")).
+                build();
+        return u;
+    }
+
+    public static Utente extract(ResultSet rs, ArrayList<Prestito> prestiti, ArrayList<Prenotazione> prenotazioni, ArrayList<Libro> interesse) throws SQLException {
+        Utente u= new Utente.UtenteBuilder().
+                email(rs.getString("u.email")).
+                password(rs.getString("u.pword")).
+                nome(rs.getString("u.nome")).
+                cognome(rs.getString("u.cognome")).
+                admin(rs.getBoolean("u.is_admin")).
+                nuovo(rs.getBoolean("u.is_nuovo")).
+                eta(rs.getInt("u.eta")).
+                genere(rs.getString("u.genere")).
+                matricola(rs.getString("u.matricola")).
+                prestiti(prestiti).
+                interessi(interesse).
+                prenotazioni(prenotazioni).
                 build();
         return u;
     }
