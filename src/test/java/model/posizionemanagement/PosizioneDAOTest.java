3fa17bad79d36c0bea81411c098e552b50785f18
Sabato Celentano <s.celentano16@studenti.unisa.it>
2022-01-17 17:09:16 +0100

    PosizioneDAOTest implementata

--- src/test/java/model/posizionemanagement/PosizioneDAOTest.java
+++ src/test/java/model/posizionemanagement/PosizioneDAOTest.java
@@ -1,4 +1,36 @@
 package model.posizionemanagement;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
 public class PosizioneDAOTest {
+    PosizioneDAO posizioneDAO;
+
+    @Before
+    public void setUp() {
+        posizioneDAO = new PosizioneDAO();
+    }
+
+    @Test
+    public void insertTest() throws SQLException {
+        Posizione p = new Posizione("Scientifica", "Piano 3");
+        posizioneDAO.insert(p);
+        Posizione p1 = posizioneDAO.doRetrieveByBibliotecaZona(p.getBiblioteca(), p.getZona());
+        assertEquals(p.getId(), p1.getId());
+        assertEquals(p.getBiblioteca(), p1.getBiblioteca());
+        assertEquals(p.getZona(), p1.getZona());
+    }
+
+    @Test
+    public void doRetrieveByBibliotecaZonaTest() throws SQLException {
+        String biblioteca = "Scientifica";
+        String zona = "Piano 2";
+        Posizione p = posizioneDAO.doRetrieveByBibliotecaZona(biblioteca, zona);
+        assertEquals(biblioteca, p.getBiblioteca());
+        assertEquals(zona, p.getZona());
+    }
 }
